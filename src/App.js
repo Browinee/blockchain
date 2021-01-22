d543e13bdb830738dfd416b57f2342faa7179a32
justin <>
2021-01-22 23:43:14 +0800

    feat: make period

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import LineAreaChart from "./component/LineAreaChart";
-
+const ChartData = {
+  data: [
+    { x: 1, y: 10 },
+    { x: 2, y: 5 },
+    { x: 3, y: 15 },
+    { x: 4, y: 10 },
+    { x: 6, y: 20 },
+    { x: 7, y: 20 },
+    { x: 8, y: 20 },
+    { x: 9, y: 20 },
+    { x: 10, y: 17 },
+  ],
+  xAxisTicks:[2,4,5,6,8,10],
+  yAxisTicks: [0, 2,5,6,9,15, 20, 25,30],
+  yAxisPeriodStart: 50,
+  yAxisPeriodEnd: 250,
+  xAxisLatestValueRefereceLine: 100,
+  xMax: 15,
+  yMax: 30,
+  periodStart: [6],
+  periodStop: [10],
+};
 function App() {
-  const chartData = {
-    data: [
-      { x: 1, y: 10 },
-      { x: 2, y: 5 },
-      { x: 3, y: 15 },
-      { x: 4, y: 10 },
-      { x: 5, y: 20 },
-    ],
-    circleSize: [2],
-    yAxisPeriodStart: 50,
-    yAxisPeriodEnd: 250,
-    xAxisLatestValueRefereceLine: 100,
-  };
+  const [chartData, setChart] = useState(ChartData);
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     let i = 11;
+  //     setChart(old => {
+  //       i += 1;
+  //       const newData = [...old.data];
+  //       newData.push({x: i, y: Math.random() * 30});
+  //       for (let j = 1 ; j <4; j++){
+  //         i+=1;
+  //         newData.push({x: i, y: null})
+
+  //       }
+  //       console.log("newData", newData);
+  //       const newXAxisTicks = [...old.xAxisTicks];
+  //       newXAxisTicks.push(i);
+  //       return {
+  //         ...old,
+  //         data: newData,
+  //         xAxisTicks:newXAxisTicks,
+  //       }
+  //     })
+  //   }, 20000)
+  //   return () => {
+  //     clearInterval(id);
+  //   }
+  // }, []);
   return (
     <div className="App">
       <LineAreaChart chartData={chartData} />
--- src/component/LineAreaChart/index.js
+++ src/component/LineAreaChart/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   HorizontalGridLines,
   VerticalGridLines,
@@ -15,44 +15,82 @@ import "react-vis/dist/style.css";
 import PropTypes from "prop-types";
 import "./style.css";
 
+const config = {
+  circleSize: [2],
+  chartWidth: 650,
+  chartHeight: 350,
+  offset: {
+    markerOffset: 10,
+    periodHeightOffset: 50
+  }
+}
+
+
 function LineAreaChart(props) {
   const {
     data,
-    circleSize,
     yAxisPeriodStart,
     yAxisPeriodEnd,
     xAxisLatestValueRefereceLine,
+    xAxisTicks,
+    yAxisTicks,
+    xMax,
+    yMax,
+    periodStart,
+    periodStop,
   } = props.chartData;
-  const xTickFormatHandler = (v) => v;
+  const {circleSize, chartWidth, chartHeight, offset: {markerOffset, periodHeightOffset}} = config;
+  const xTickFormatHandler = (v) => `Start: 06${v}`
   const yTickFormatHandler = (v) => v;
   const yAxisPeriodTitle = (position, value) => {
     return `${position}: ${value}`;
   };
+  const [referencePosition, setReference] = useState(0);
+  const getReferenceLinePosition = () => {
+    const marker = document.querySelector(".mark-series > circle");
+    if(marker) {
+      const cy = +marker.attributes.cy.value;
+      setReference(cy + markerOffset);
+    }
+  }
+  const [periodHeight, setPeriodHeight] = useState(chartHeight - periodHeightOffset);
+  useEffect(() => {
+    setPeriodHeight(chartHeight - periodHeightOffset);
+  }, [chartHeight]);
+  useEffect(() => {
+    getReferenceLinePosition();
+  }, [data]);
+
   return (
     <div>
-    <XYPlot height={300} width={300}>
-      <VerticalGridLines
-        style={{
-          backgroundColor: "black",
-        }}
+    <XYPlot height={chartHeight} width={chartWidth} >
+      {/* 用AreaSeries畫出背景，要放在第一個 */}
+    <AreaSeries
+        className="area-series"
+        curve="linear"
+        data={[{x:0, y:yMax}, {x:xMax, y:yMax}]}
+        fill="#2B2B2B"
+        stroke="transparent"
       />
-      <HorizontalGridLines />
+      <VerticalGridLines style={{stroke: "black"}} className="verticalGridLine" />
+      <HorizontalGridLines style={{stroke: "black"}}/>
       <XAxis
         tickSizeOuter={0}
         tickSizeInner={0}
         tickFormat={xTickFormatHandler}
-        tickValues={[1, 2, 3, 4, 5, 6, 7]}
-        tickTotal={20}
+        tickValues={xAxisTicks}
         style={{
           line: {
             stroke: "black",
           },
         }}
       />
+
       <YAxis
         tickSizeInner={0}
         tickSizeOuter={0}
         tickFormat={yTickFormatHandler}
+        tickValues={yAxisTicks}
         style={{
           line: {
             stroke: "black",
@@ -60,78 +98,108 @@ function LineAreaChart(props) {
         }}
       />
 
-      <AreaSeries
-        className="area-series-example"
-        curve="linear"
-        data={data}
-        fill="#FBD241"
-        stroke="transparent"
-      />
+
       <LineSeries stroke="#D98C14" data={data} />
       {/* 為了畫出配合x軸畫出剩餘空白的部分*/}
-      <LineSeries
+      {/* <LineSeries
         stroke="transparent"
-        data={data.concat([
-          { x: 6, y: 20 },
-          { x: 7, y: 29 },
-        ])}
-      />
+        data={emptyData}
+      /> */}
 
 
       {/* 為了畫出區隔線*/}
-      <YAxis
-        left={yAxisPeriodStart}
+      {/* <YAxis
+        left={periodStartPosition}
         position="end"
         hideTicks
-        title={yAxisPeriodTitle("Start", "06:00")}
+        title={yAxisPeriodTitle("Start", periodStart)}
         style={{
           title: { transform: "translate(-8px, 5px)" },
           line: {
-            stroke: "black",
+            // stroke: "white",
           },
         }}
       />
       <YAxis
-        left={yAxisPeriodEnd}
+        left={periodEndPosition}
         position="end"
         hideTicks
-        title={yAxisPeriodTitle("Stop", "06:00")}
+        title={yAxisPeriodTitle("Stop", periodStop)}
         style={{
           title: { transform: "translate(-8px, 5px)" },
           line: {
-            stroke: "black",
+            // stroke: "white",
           },
         }}
+      /> */}
+
+      <XAxis
+        className="XAxisPeriodStart"
+        tickSizeOuter={0}
+        tickSizeInner={periodHeight}
+        tickFormat={xTickFormatHandler}
+        tickValues={periodStart}
+        hideLine
+        style={{
+          text: {
+            stroke: "white",
+            transform: `translate(-40px, ${-periodHeight + 20}px)`,
+          }
+        }}
       />
+      <XAxis
+        className="XAxisPeriodEnd"
+        tickSizeOuter={0}
+        tickSizeInner={periodHeight}
+        tickFormat={xTickFormatHandler}
+        tickValues={periodStop}
+        position="start"
+        hideLine
+        style={{
+          text: {
+            stroke: "white",
+            transform: `translate(-40px, ${-periodHeight + 20}px)`,
+          }
+        }}
+      />
+
 
        {/* 為了畫出最新值的參考線*/}
 
        <XAxis
         hideTicks
-        top={100}
+        top={referencePosition}
         style={{
           line: {
-            stroke: "black",
+            stroke: "white",
             strokeDasharray: "4"
           },
         }}
       />
-       <MarkSeries
-        className="mark-series-example"
-        sizeRange={circleSize}
-        data={data.slice(-1)}
-        fill="white"
-      />
+        <MarkSeries
+          className="mark-series"
+          sizeRange={circleSize}
+          data={data.slice(-1)}
+          fill="white"
+          stroke="white"
+          />
     </XYPlot>
     </div>
   );
 }
 LineAreaChart.propTypes = {
-  data: [],
-  circleSize: PropTypes.arrayOf(PropTypes.number),
+  data: PropTypes.array,
+  xAxisTicks: PropTypes.array,
+  yAxisTicks: PropTypes.array,
   yAxisPeriodStart: PropTypes.number,
   yAxisPeriodEnd: PropTypes.number,
   xAxisLatestValueRefereceLine: PropTypes.number,
+  highHandler: PropTypes.func,
+  lowHandler: PropTypes.func,
+  xMax: PropTypes.any,
+  yMax: PropTypes.any,
+  periodStart: PropTypes.string,
+  periodStop: PropTypes.string,
 };
 
 export default LineAreaChart;
